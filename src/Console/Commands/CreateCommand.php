ae556575a4ec0d3a16662f9790193479ae68c444
Sashagm <example@example.com>
2023-06-22 00:05:57 +0500

    Добавлена консольная команда seo:install

---
+++ src/Console/Commands/CreateCommand.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Sashagm\Seo\Console\Commands;
+
+use Illuminate\Console\Command;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Artisan;
+use Sashagm\Seo\Providers\SeoServiceProvider;
+
+
+class CreateCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'seo:install';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Данная команда создаст необходимые файлы.';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $this->components->info('Установка пакета...');
+        $this->install();
+        $this->components->info('Установка завершена!');
+        
+    }
+
+    protected function install(): void
+    {
+        Artisan::call('vendor:publish', [
+            '--provider' => SeoServiceProvider::class,
+            '--force' => true,
+        ]);
+        $this->components->info('Сервис провайдер опубликован...');
+
+        Artisan::call('migrate');
+        $this->components->info('Миграции выполнены...');
+
+
+ 
+    }
+
+
+
+}
--- src/Providers/SeoServiceProvider.php
+++ src/Providers/SeoServiceProvider.php
@@ -5,6 +5,7 @@ namespace Sashagm\Seo\Providers;
 use Sashagm\Seo\Traits\SeoTrait;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
+use Sashagm\Seo\Console\Commands\CreateCommand;
 
 class SeoServiceProvider extends ServiceProvider
 {
@@ -29,6 +30,11 @@ class SeoServiceProvider extends ServiceProvider
         $this->mergeConfigFrom(
             __DIR__.'/../config/seo.php', 'seo'
         );
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                CreateCommand::class,
+            ]);
+        }
 
         $this->bootSeo();
 
